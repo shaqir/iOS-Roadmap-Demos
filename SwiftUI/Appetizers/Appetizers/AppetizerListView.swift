8cd7267da56f968b76c915d8143bf5818fd565e2
Sakir Saiyed <sakirsaiyed29@gmail.com>
2025-05-30 19:43:11 -0600

    Initial commit.

---
+++ SwiftUI/Appetizers/Appetizers/AppetizerListView.swift
@@ -0,0 +1,18 @@
+//
+//  AppetizerListView.swift
+//  Appetizers
+//
+//  Created by Sakir Saiyed on 30/05/25.
+//
+
+import SwiftUI
+
+struct AppetizerListView: View {
+    var body: some View {
+        Text("Appetizer List View")
+    }
+}
+
+#Preview {
+    AppetizerListView()
+}
---
+++ SwiftUI/Appetizers/Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Sakir Saiyed on 30/05/25.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AppetizerTabView()
+        }
+    }
+}
