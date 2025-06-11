56e79665be85fffee30fe9b38128bc17e991a4c0
SakirAli Saiyed <SakirSaiyed29@gmail.com>
2025-06-11 13:41:30 -0600

    Alamofire demo in SwiftUI

---
+++ Networking/AlmofireDemoInSwiftUI/AlmofireDemoInSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AlmofireDemoInSwiftUI
+//
+//  Created by Sakir Saiyed on 2025-06-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
