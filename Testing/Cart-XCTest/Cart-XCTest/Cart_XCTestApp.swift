e560e815fe9509fd89429d23d985728b4b9946cb
SakirAli Saiyed <SakirSaiyed29@gmail.com>
2025-06-12 16:32:49 -0600

    XCTest cases and code coverage tested for cart demo (#24)
    * Readme updated
    * Improved structure, added test cases and refactored code.
    * XCTest cases and code coverage tested for the cart demo.

---
+++ Testing/Cart-XCTest/Cart-XCTest/Cart_XCTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cart_XCTestApp.swift
+//  Cart-XCTest
+//
+//  Created by Sakir Saiyed on 2025-06-12.
+//
+
+import SwiftUI
+
+@main
+struct Cart_XCTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
