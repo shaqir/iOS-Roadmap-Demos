abd4b71c9273983e5473f8e8da0306077a67cac2
SakirAli Saiyed <SakirSaiyed29@gmail.com>
2025-06-12 19:04:53 -0600

    Async demo with XCTest (#33)

---
+++ Testing/AsyncDemo-XCTest/AsyncDemo-XCTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AsyncDemo-XCTest
+//
+//  Created by Sakir Saiyed on 2025-06-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
